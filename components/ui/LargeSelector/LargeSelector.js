45fb9fa22f3a8df3ac05ec66998f8e989981b4bb
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-05 16:37:39 +0100

    Make Link component the parent element

--- components/ui/LargeSelector/LargeSelector.js
+++ components/ui/LargeSelector/LargeSelector.js
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 const LargeSelector = (props) => {
-    const { link, title, test } = props;
+    const { link, title } = props;
 
     return (
-        <div className="flex items-center justify-center text-5xl flex-1 hover:bg-teal-600 cursor-pointer duration-500 ease-in-out hover:text-yellow-red-900">
-            <Link href={link}>{title}</Link>
-        </div>
+        <Link href={link}>
+            <div className="flex items-center justify-center text-5xl flex-1 hover:bg-teal-600 cursor-pointer duration-500 ease-in-out hover:text-yellow-red-900">
+                {title}
+            </div>
+        </Link>
     );
 };
 
