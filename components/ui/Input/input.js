851eeb390fb329e270db86e3d02c48c0606bb379
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-05 18:03:10 +0100

    Update input component

--- components/ui/Input/input.js
+++ components/ui/Input/input.js
@@ -1,12 +1,15 @@
 const input = (props) => {
-    const { title } = props;
+    const { title, changed, id, value } = props;
 
     return (
-        <label className="block">
+        <label htmlFor={id} className="block cursor-pointer">
             <span className="text-gray-700 text-sm">{title}</span>
             <input
+                id={id}
                 type="text"
                 class="form-input block mt-1 rounded-md border-gray-300 shadow-sm focus:border-teal-500 focus:ring focus:ring-teal-400 focus:ring-opacity-50"
+                onChange={changed}
+                value={value}
             />
         </label>
     );
