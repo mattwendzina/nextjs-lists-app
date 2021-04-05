855f263f443d4e6ff006e9f3514d88e28c5e9efe
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-05 16:38:24 +0100

    Add Input component to createList page

--- pages/createList.js
+++ pages/createList.js
@@ -1,7 +1,10 @@
+import Input from '../components/ui/Input/input';
+
 const createList = () => {
     return (
         <div>
             <h2>Create List</h2>
+            <Input title="List name" />
         </div>
     );
 };
