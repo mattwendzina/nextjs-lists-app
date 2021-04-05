90ef4d5e7245a4fd993dba5a50ed865d8af170e5
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-05 14:50:29 +0100

    Add proptypes for largeSelector component

--- components/ui/LargeSelector/LargeSelector.js
+++ components/ui/LargeSelector/LargeSelector.js
@@ -1,7 +1,8 @@
+import PropTypes from 'prop-types';
 import Link from 'next/link';
 
-const Hero = (props) => {
-    const { link, title } = props;
+const LargeSelector = (props) => {
+    const { link, title, test } = props;
 
     return (
         <div className="flex items-center justify-center text-5xl flex-1 hover:bg-teal-600 cursor-pointer duration-500 ease-in-out hover:text-yellow-red-900">
@@ -10,4 +11,9 @@ const Hero = (props) => {
     );
 };
 
-export default Hero;
+LargeSelector.propTypes = {
+    link: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+};
+
+export default LargeSelector;
