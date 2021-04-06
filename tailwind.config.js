705ff03e1d378f3273861080e473418e0697d605
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-06 21:15:52 +0100

    Remove custom outline

--- tailwind.config.js
+++ tailwind.config.js
@@ -47,9 +47,6 @@ module.exports = {
             fontFamily: {
                 lexend: ['Lexend', 'Helvetica', 'Arial', 'sans-serif'],
             },
-            outline: {
-                blue: '2px solid #0000ff',
-            },
         },
     },
     variants: {
