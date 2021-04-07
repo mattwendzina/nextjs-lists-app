4506b01146b23fb79154dd9326a3c104a41804f6
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-07 21:56:28 +0100

    First steps on editing an item with the store

--- components/Lists/ListItem.js
+++ components/Lists/ListItem.js
@@ -1,12 +1,38 @@
+import { useState, useContext } from 'react';
 import { FiDelete } from 'react-icons/fi';
 import classes from './listItem.module.css';
 
+import ItemsContext from '../../store/items-context';
+import input from '../ui/Input/input';
+
 const ListItem = ({ title, removeItem, id }) => {
+    const [updateItem, toggleUpdateItem] = useState(false);
+    const itemsCtx = useContext(ItemsContext);
+
     const liClassNames = `${classes.listItem} relative group cursor-pointer mx-auto w-max flex justify-center items-center`;
+
+    const editItem = (e) => {
+        e.preventDefault();
+        toggleUpdateItem(false);
+    };
+
+    const updateText = (value) => {
+        itemsCtx.updateItem(value, id);
+    };
 
     return (
         <li className={liClassNames}>
-            {title}
+            {!updateItem ? (
+                <p onClick={() => toggleUpdateItem(true)}>{title}</p>
+            ) : (
+                <form onSubmit={editItem}>
+                    <input
+                        value={title}
+                        onChange={(e) => updateText(e.target.value)}
+                    />
+                    <button>Update</button>
+                </form>
+            )}
             <FiDelete className={classes.icon} onClick={() => removeItem(id)} />
         </li>
     );
