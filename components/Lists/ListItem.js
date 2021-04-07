9d31f582a71020f7a7dd7284cf24a155ae5f01f6
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-07 19:40:34 +0100

    ui updates and add delete method

--- components/Lists/ListItem.js
+++ components/Lists/ListItem.js
@@ -1,5 +1,14 @@
-const ListItem = (props) => {
-    return <li>{props.title}</li>;
-};
+import { FiDelete } from 'react-icons/fi';
+import classes from './listItem.module.css';
 
+const ListItem = ({ title, removeItem, id }) => {
+    const liClassNames = `${classes.listItem} relative group cursor-pointer mx-auto w-max flex justify-center items-center`;
+
+    return (
+        <li className={liClassNames}>
+            {title}
+            <FiDelete className={classes.icon} onClick={() => removeItem(id)} />
+        </li>
+    );
+};
 export default ListItem;
