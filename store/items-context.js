0a1f82006d1bbaefb6e8ad10f7d258c53aefd9d1
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-09 07:49:38 +0100

    Update logic to deal with user deleting a word

--- store/items-context.js
+++ store/items-context.js
@@ -23,14 +23,30 @@ export const ItemsContextProvider = (props) => {
     };
 
     const updateItemHandler = (value, id) => {
-        setItems(
-            items.map((item) => {
-                if (item['id'] === id) {
-                    return { ...item, name: value };
-                }
-                return item;
-            })
-        );
+        setItems((prevState) => {
+            const updatedItems = items
+                .map((item, idx) => {
+                    if (item['id'] === id) {
+                        // Convulted but... Need to deal with scenario where a user has deleted a word. At that state,
+                        // they may start typing a new one, or click away expecting it to be deleted. We need to look at the prevState
+                        // so we can compare with the next incoming value. IE, if prevState is an empty string, and the current value is
+                        // an empty string then they must want to delete - in which case, come into this if block. If the value is a
+                        // character though, then they must be adding a new item.
+                        if (prevState[idx].name === '' && value === '') {
+                            return { ...item, name: value, removeItem: true };
+                        }
+                        return { ...item, name: value };
+                    }
+                    return item;
+                })
+                .filter((item) => {
+                    if (item.removeItem) {
+                        return false;
+                    }
+                    return item;
+                });
+            return updatedItems;
+        });
     };
 
     const context = {
