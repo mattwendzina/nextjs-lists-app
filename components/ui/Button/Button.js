324bd33d8052b18381482558267e4c70b93d1bcd
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-11 08:55:51 +0100

    Add type, and add conditionals for class styling

--- components/ui/Button/Button.js
+++ components/ui/Button/Button.js
@@ -1,10 +1,30 @@
+import PropTypes from 'prop-types';
+
 const Button = (props) => {
-    const { title } = props;
+    const { title, click, classes, type } = props;
+
+    const initialClasses = [
+        'bg-teal-500 rounded-md p-2 m-2 text-white hover:text-yellow-red-900 transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-yellow-red-900 focus:ring-opacity-50 shadow-lg',
+    ];
+
+    if (classes) {
+        initialClasses.push(...classes);
+    }
+
+    const combinedClasses = initialClasses.join(' ');
+
     return (
-        <button className="bg-teal-500 rounded-md p-2 mt-2.5 text-white hover:text-yellow-red-900 transition duration-300 ease-in-out focus:outline-none focus:ring focus:ring-yellow-red-900 focus:ring-opacity-50">
+        <button type={type} onClick={click} className={combinedClasses}>
             {title}
         </button>
     );
 };
 
 export default Button;
+
+Button.propTypes = {
+    title: PropTypes.string.isRequired,
+    click: PropTypes.func,
+    classes: PropTypes.array,
+    type: PropTypes.string,
+};
