0e8938db052509753e5ba198bbc8ca7435afd11f
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-11 09:07:25 +0100

    Add list page

---
+++ pages/lists/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import AllLists from '../../components/AllLists/index';
+
+const lists = () => {
+    return (
+        <Fragment>
+            <AllLists />
+        </Fragment>
+    );
+};
+
+export default lists;
