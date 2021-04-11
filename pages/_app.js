b442424e65f4d6a8f44a079d126d59895bddfb83
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-11 08:49:49 +0100

    Add in the listsContextProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import Layout from '../components/Layout/Layout';
 import { ItemsContextProvider } from '../store/items-context';
+import { ListsContextProvider } from '../store/lists-context';
 import '../styles/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
     return (
         <ItemsContextProvider>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
+            <ListsContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ListsContextProvider>
         </ItemsContextProvider>
     );
 }
