719f9610fe8cf9561004aac96085587ff29fc290
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-11 09:34:39 +0100

    Comment out config code

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production';
+// const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev
-    ? 'http://localhost:3000'
-    : 'https://mw-listsapp.vercel.app/';
+// export const server = dev
+//     ? 'http://localhost:3000'
+//     : 'https://mw-listsapp.vercel.app/';
