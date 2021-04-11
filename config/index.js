dfc922439b717c563237d8955f7a4afe874a79b0
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-11 09:25:22 +0100

    Update server url for production

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production';
 
 export const server = dev
     ? 'http://localhost:3000'
-    : 'https://nextjs-lists-app.vercel.app/';
+    : 'https://mw-listsapp.vercel.app/';
