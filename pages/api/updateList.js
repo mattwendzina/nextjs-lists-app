5d8e9bba68e9337474b575f7884a860e94786b74
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-18 11:37:30 +0100

    Remove console.log statement

--- pages/api/updateList.js
+++ pages/api/updateList.js
@@ -2,7 +2,6 @@ import { MongoClient } from 'mongodb';
 
 export default async (req, res) => {
     if (req.method === 'POST') {
-        console.log('body', req.body);
         const connectionString =
             'mongodb+srv://mattwendzina:bXKcltQ6Ovq1jl8g@cluster0.a1qam.mongodb.net/listsDatabase?retryWrites=true&w=majority';
 
