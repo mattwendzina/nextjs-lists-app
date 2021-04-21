352dcc6e2689b5c55f5458e6b6b5e2f595ab8c32
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-21 06:59:51 +0100

    Add sidedrawer component

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,12 @@
-import { Fragment } from 'react';
 import Navbar from './Navbar/Navbar';
+import SideDrawer from './SideDrawer/SideDrawer';
 
 const Layout = (props) => {
     return (
         <div className="flex flex-col h-screen">
             <div>
                 <Navbar />
+                <SideDrawer />
             </div>
             <main className="bg-gainsboro-50 flex-1 h-full">
                 <div className="bg-white mx-auto md:max-w-5xl h-full">
