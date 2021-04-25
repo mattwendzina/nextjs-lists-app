3506b6c7cad53094ee2df9389f43004bd295676b
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-25 09:46:51 +0100

    Add error handling for database connection and tidy up

--- pages/api/allLists.js
+++ pages/api/allLists.js
@@ -8,17 +8,27 @@ export default async (req, res) => {
     const connectionString =
         'mongodb+srv://mattwendzina:bXKcltQ6Ovq1jl8g@cluster0.a1qam.mongodb.net/listsDatabase?retryWrites=true&w=majority';
 
-    const client = await MongoClient.connect(connectionString);
+    let client;
+    try {
+        client = await MongoClient.connect(connectionString);
+    } catch (e) {
+        res.status(500).json({
+            message: `Failed to connect to server! - ${e.message}`,
+        });
+        return;
+    }
 
     const db = client.db();
 
+    let result;
     try {
-        const result = await db.collection('lists').find().toArray();
-        res.status(201).json({ allLists: result });
-        client.close();
+        result = await db.collection('lists').find().toArray();
     } catch (e) {
-        console.log(e);
         client.close();
+        res.status(500).json({ message: 'Failed to retrieve lists!' });
         return;
     }
+
+    client.close();
+    res.status(201).json({ message: 'Lists received', allLists: result });
 };
