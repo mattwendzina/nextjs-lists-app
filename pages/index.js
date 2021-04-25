7ec4ddc451f9c8ce1f5b67b4ff5aebb9b723c0d0
Matthew Wendzina <mattwendzina@gmail.com>
2021-04-25 09:46:16 +0100

    Add getServerSideProps and error handling

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,39 @@
 import { Fragment } from 'react';
+import Error from 'next/error';
 import Head from 'next/head';
 import Homepage from '../components/Homepage/Homepage';
+import { getAllLists } from '../helpers/api-utils';
 
-export default function Home() {
+export default function Home({ error, results }) {
+    if (error) {
+        console.error('ERROR: ', error);
+        return <Error statusCode={error.message} />;
+    }
+
     return (
         <Fragment>
             <Head>
                 <title>Matt Wendzina</title>
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Homepage />
+            <Homepage allLists={results.allLists} />
         </Fragment>
     );
+}
+
+export async function getServerSideProps() {
+    let res = null;
+    let errorCode = false;
+    try {
+        res = await getAllLists();
+    } catch (e) {
+        errorCode = e.props;
+    }
+
+    return {
+        props: {
+            error: errorCode,
+            results: res,
+        },
+    };
 }
