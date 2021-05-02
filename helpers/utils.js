dc1e1e2588cf5b2be0551f6ee21d6d239d98733e
Matthew Wendzina <mattwendzina@gmail.com>
2021-05-02 08:30:38 +0100

    Filter out items with no value

--- helpers/utils.js
+++ helpers/utils.js
@@ -9,15 +9,18 @@ export const hasItemChanged = (list, value, id) => {
 };
 
 export const modifyItems = (selectedList, property, value, id) =>
-    selectedList.items.map((item) => {
-        if (item.id === id) {
-            return {
-                ...item,
-                [property]: value,
-            };
-        }
-        return item;
-    });
+    selectedList.items
+        .map((item) => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    [property]: value,
+                };
+            }
+            return item;
+        })
+        // If item has no value then assume user wanted to delete and filter it out
+        .filter((item) => item.name !== '');
 
 export const getDate = () => {
     return moment().format('MMMM Do YYYY, h:mm:ss a');
