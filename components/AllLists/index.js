a2e865b098e67d16f23a47fa6fddc4cb4ec941bd
Matthew Wendzina <mattwendzina@gmail.com>
2021-11-26 15:05:22 +0000

    Refactor allLists code to use serverSide rendering

--- components/AllLists/index.js
+++ components/AllLists/index.js
@@ -1,67 +1,33 @@
-import { useEffect, useContext, useState } from 'react';
-import Error from 'next/error';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import ListsContext from '../../store/lists-context';
 
-const AllLists = () => {
-    const [error, setError] = useState();
-    const listsCtx = useContext(ListsContext);
-
-    useEffect(async () => {
-        if (listsCtx.allLists.length === 0) {
-            const response = await fetch('/api/allLists', {
-                method: 'GET',
-            });
-            if (!response.ok) {
-                const data = await response.json();
-                setError({
-                    message: 'Failed to get lists!',
-                    data: data.message,
-                });
-                return;
-            }
-            const { allLists } = await response.json();
-            listsCtx.setLists(allLists);
-        }
-    }, []);
-
-    if (error) {
-        return <Error statusCode={error.message} />;
-    }
-
-    if (listsCtx.allLists.length === 0) {
-        return <p className="text-center text-xl"> Loading...</p>;
-    }
-
-    return (
-        <div className="text-center w-max mx-auto ">
-            <ul>
-                {listsCtx.allLists.map((list) => {
-                    return (
-                        <motion.li
-                            initial={{ opacity: 0 }}
-                            animate={{ opacity: 1 }}
-                            key={list._id}
-                            className="text-2xl p-2 cursor-pointer hover:text-yellow-red-900 transition duration-200 ease-in-out"
+const AllLists = ({ allLists }) => (
+    <div className="text-center w-max mx-auto ">
+        <ul>
+            {allLists.map((list) => {
+                return (
+                    <motion.li
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        key={list._id}
+                        className="text-2xl p-2 cursor-pointer hover:text-yellow-red-900 transition duration-200 ease-in-out"
+                    >
+                        <Link
+                            href={{
+                                pathname: `/lists/[title]/[listId]`,
+                                query: {
+                                    listId: list._id,
+                                    title: list.title,
+                                },
+                            }}
                         >
-                            <Link
-                                href={{
-                                    pathname: `/lists/[title]/[listId]`,
-                                    query: {
-                                        listId: list._id,
-                                        title: list.title,
-                                    },
-                                }}
-                            >
-                                {list.title}
-                            </Link>
-                        </motion.li>
-                    );
-                })}
-            </ul>
-        </div>
-    );
-};
+                            {list.title}
+                        </Link>
+                    </motion.li>
+                );
+            })}
+        </ul>
+    </div>
+);
 
 export default AllLists;
--- pages/lists/index.js
+++ pages/lists/index.js
@@ -1,12 +1,35 @@
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import AllLists from '../../components/AllLists/index';
+import { getAllLists } from '../../helpers/api-utils';
+import ListsContext from '../../store/lists-context';
 
-const lists = () => {
+const lists = ({ allLists }) => {
+    const listsCtx = useContext(ListsContext);
+    let lists;
+    listsCtx.allLists.length === 0
+        ? (lists = allLists)
+        : (lists = listsCtx.allLists);
+
     return (
         <Fragment>
-            <AllLists />
+            <AllLists allLists={lists} />
         </Fragment>
     );
 };
+
+export async function getServerSideProps() {
+    let result = null;
+    let errorCode = false;
+    try {
+        result = await getAllLists();
+    } catch (e) {
+        errorCode = e.props;
+    }
+    return {
+        props: {
+            allLists: result && result.allLists,
+        },
+    };
+}
 
 export default lists;
