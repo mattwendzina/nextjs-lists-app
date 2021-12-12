5d0d04665b7ab039ab9e9a033fb9e98f3bbc021b
Matthew Wendzina <mattwendzina@gmail.com>
2021-12-12 16:06:43 +0000

    Remove the settings of lists from Homepage

--- components/Homepage/Homepage.js
+++ components/Homepage/Homepage.js
@@ -3,19 +3,11 @@ import ListsContext from '../../store/lists-context';
 
 import LargeSelector from '../ui/LargeSelector/LargeSelector';
 
-const Homepage = ({ allLists }) => {
-    const listsCtx = useContext(ListsContext);
-
-    useEffect(() => {
-        listsCtx.setLists(allLists);
-    }, []);
-
-    return (
-        <div className="border border-gray-200 shadow-md p-1 flex flex-col md:flex-row md:align-center md:justify-center h-full">
-            <LargeSelector link="/lists" title="Select a List" />
-            <LargeSelector link="/createList" title="Create a List" />
-        </div>
-    );
-};
+const Homepage = () => (
+    <div className="border border-gray-200 shadow-md p-1 flex flex-col md:flex-row md:align-center md:justify-center h-full">
+        <LargeSelector link="/lists" title="Select a List" />
+        <LargeSelector link="/createList" title="Create a List" />
+    </div>
+);
 
 export default Homepage;
